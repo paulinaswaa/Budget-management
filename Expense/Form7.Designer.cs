01df7b336e1c60329528b1919b7b6ab856d1b2ca
paulinaswaa <124894941+paulinaswaa@users.noreply.github.com>
2023-03-21 23:35:23 +0100

    Adding new user to database

--- Expense/Form7.Designer.cs
+++ Expense/Form7.Designer.cs
@@ -28,12 +28,202 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form7));
+            this.pictureBox7 = new System.Windows.Forms.PictureBox();
+            this.addbutton = new System.Windows.Forms.Button();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label12 = new System.Windows.Forms.Label();
+            this.passwordadd = new System.Windows.Forms.TextBox();
+            this.phoneadd = new System.Windows.Forms.TextBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.dateadd = new System.Windows.Forms.DateTimePicker();
+            this.label11 = new System.Windows.Forms.Label();
+            this.usernameadd = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.adressadd = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox7)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox7
+            // 
+            this.pictureBox7.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox7.Image")));
+            this.pictureBox7.Location = new System.Drawing.Point(400, 17);
+            this.pictureBox7.Name = "pictureBox7";
+            this.pictureBox7.Size = new System.Drawing.Size(35, 38);
+            this.pictureBox7.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox7.TabIndex = 130;
+            this.pictureBox7.TabStop = false;
+            this.pictureBox7.Click += new System.EventHandler(this.pictureBox7_Click);
+            // 
+            // addbutton
+            // 
+            this.addbutton.Font = new System.Drawing.Font("Bahnschrift", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.addbutton.Location = new System.Drawing.Point(137, 541);
+            this.addbutton.Name = "addbutton";
+            this.addbutton.Size = new System.Drawing.Size(163, 38);
+            this.addbutton.TabIndex = 129;
+            this.addbutton.Text = "Add";
+            this.addbutton.UseVisualStyleBackColor = true;
+            this.addbutton.Click += new System.EventHandler(this.addbutton_Click);
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Font = new System.Drawing.Font("Bahnschrift Light", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label8.Location = new System.Drawing.Point(86, 354);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(91, 22);
+            this.label8.TabIndex = 127;
+            this.label8.Text = "Password";
+            // 
+            // label12
+            // 
+            this.label12.AutoSize = true;
+            this.label12.Font = new System.Drawing.Font("Bahnschrift Light", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label12.Location = new System.Drawing.Point(162, 17);
+            this.label12.Name = "label12";
+            this.label12.Size = new System.Drawing.Size(129, 34);
+            this.label12.TabIndex = 128;
+            this.label12.Text = "Add user";
+            // 
+            // passwordadd
+            // 
+            this.passwordadd.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.passwordadd.Location = new System.Drawing.Point(89, 389);
+            this.passwordadd.Name = "passwordadd";
+            this.passwordadd.Size = new System.Drawing.Size(271, 30);
+            this.passwordadd.TabIndex = 126;
+            // 
+            // phoneadd
+            // 
+            this.phoneadd.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.phoneadd.Location = new System.Drawing.Point(89, 282);
+            this.phoneadd.Name = "phoneadd";
+            this.phoneadd.Size = new System.Drawing.Size(271, 30);
+            this.phoneadd.TabIndex = 124;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Bahnschrift Light", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label7.Location = new System.Drawing.Point(85, 247);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(61, 22);
+            this.label7.TabIndex = 125;
+            this.label7.Text = "Phone";
+            // 
+            // dateadd
+            // 
+            this.dateadd.Font = new System.Drawing.Font("Bahnschrift", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.dateadd.Format = System.Windows.Forms.DateTimePickerFormat.Short;
+            this.dateadd.Location = new System.Drawing.Point(89, 192);
+            this.dateadd.Name = "dateadd";
+            this.dateadd.Size = new System.Drawing.Size(271, 28);
+            this.dateadd.TabIndex = 132;
+            // 
+            // label11
+            // 
+            this.label11.AutoSize = true;
+            this.label11.Font = new System.Drawing.Font("Bahnschrift Light", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label11.Location = new System.Drawing.Point(85, 153);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(48, 22);
+            this.label11.TabIndex = 131;
+            this.label11.Text = "Date";
+            // 
+            // usernameadd
+            // 
+            this.usernameadd.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.usernameadd.Location = new System.Drawing.Point(89, 103);
+            this.usernameadd.Name = "usernameadd";
+            this.usernameadd.Size = new System.Drawing.Size(271, 30);
+            this.usernameadd.TabIndex = 133;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Bahnschrift Light", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label1.Location = new System.Drawing.Point(85, 68);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(94, 22);
+            this.label1.TabIndex = 134;
+            this.label1.Text = "Username";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Bahnschrift Light", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label2.Location = new System.Drawing.Point(86, 449);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(68, 22);
+            this.label2.TabIndex = 136;
+            this.label2.Text = "Adress";
+            // 
+            // adressadd
+            // 
+            this.adressadd.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.adressadd.Location = new System.Drawing.Point(89, 484);
+            this.adressadd.Name = "adressadd";
+            this.adressadd.Size = new System.Drawing.Size(271, 30);
+            this.adressadd.TabIndex = 135;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Bahnschrift", 7.8F, System.Drawing.FontStyle.Underline, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label3.Location = new System.Drawing.Point(201, 589);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(35, 16);
+            this.label3.TabIndex = 137;
+            this.label3.Text = "Back";
+            // 
+            // Form7
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(456, 614);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.adressadd);
+            this.Controls.Add(this.usernameadd);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.dateadd);
+            this.Controls.Add(this.label11);
+            this.Controls.Add(this.pictureBox7);
+            this.Controls.Add(this.addbutton);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label12);
+            this.Controls.Add(this.passwordadd);
+            this.Controls.Add(this.phoneadd);
+            this.Controls.Add(this.label7);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Form7";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Form7";
+            this.Load += new System.EventHandler(this.Form7_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox7)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox7;
+        private System.Windows.Forms.Button addbutton;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label12;
+        private System.Windows.Forms.TextBox passwordadd;
+        private System.Windows.Forms.TextBox phoneadd;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.DateTimePicker dateadd;
+        private System.Windows.Forms.Label label11;
+        private System.Windows.Forms.TextBox usernameadd;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox adressadd;
+        private System.Windows.Forms.Label label3;
     }
 }
--- Expense/Form7.cs
+++ Expense/Form7.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Data.SqlClient;
 
 namespace Expense
 {
@@ -15,6 +16,53 @@ namespace Expense
         public Form7()
         {
             InitializeComponent();
+        }
+
+        private void Form7_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void pictureBox7_Click(object sender, EventArgs e)
+        {
+            this.Close();
+
+        }
+        SqlConnection connection = new SqlConnection(@"Data Source=DESKTOP-HR20LU4;Initial Catalog=Expenses;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False");
+        private void Clear()
+        {
+            usernameadd.Text = string.Empty;
+            passwordadd.Text = string.Empty;
+            adressadd.Text = string.Empty;
+            phoneadd.Text = string.Empty;
+        }
+        private void addbutton_Click(object sender, EventArgs e)
+        {
+            if(usernameadd.Text==" " || phoneadd.Text==" " || passwordadd.Text=="" || adressadd.Text=="")
+            {
+                MessageBox.Show("Complete the data!");
+            }
+            else
+            {
+                try
+                {
+                    connection.Open();
+                    SqlCommand command = new SqlCommand("insert into tbl_User(User_Name, User_Date, User_Phone, User_Password, User_Adress)values (@USERN, @USERD, @USERP, @USERPW, @USERA", connection);
+                    command.Parameters.AddWithValue("@USERN", usernameadd.Text);
+                    command.Parameters.AddWithValue("@USERD", dateadd.Value.Date);
+                    command.Parameters.AddWithValue("@USERP", phoneadd.Text);
+                    command.Parameters.AddWithValue("@USERPW", passwordadd.Text);
+                    command.Parameters.AddWithValue("@USERA", adressadd.Text);
+                    command.ExecuteNonQuery();
+                    MessageBox.Show("User added correctly!");
+                    connection.Close();
+                    Clear();
+
+                } catch(Exception Ex) { 
+                    MessageBox.Show(Ex.Message);
+                   
+                }
+            }
         }
     }
 }
