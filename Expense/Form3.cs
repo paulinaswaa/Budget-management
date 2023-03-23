c4996a8054e5aa4b06ea3ea6163098fe60197515
paulinaswaa <124894941+paulinaswaa@users.noreply.github.com>
2023-03-23 11:51:20 +0100

    incomev2

--- Expense/Form3.cs
+++ Expense/Form3.cs
@@ -53,34 +53,34 @@ namespace Expense
 
         private void button1_Click(object sender, EventArgs e)
         {
-            if (incomename.Text == " " || incomeamount.Text == " " || incomecategories.SelectedIndex == -1 || incomedescription.Text == "")
-            {
-                MessageBox.Show("Complete the data!");
-            }
-            else
-            {
-                try
-                {
-                    connection.Open();
-                    SqlCommand command = new SqlCommand("insert into tbl_Income(Income_name, Income_Amount, Income_Category, Income_Date, Income_Description, Income_User)values (@IncomeName, @IncomeAmount, @IncomeCategory, @IncomeDate, @IncomeDescription, @IncomeUser)", connection);
-                    command.Parameters.AddWithValue("@IncomeName", incomename.Text);
-                    command.Parameters.AddWithValue("@IncomeAmount", incomeamount.Text);
-                    command.Parameters.AddWithValue("@IncomeCategory", incomecategories.SelectedItem.ToString());
-                    command.Parameters.AddWithValue("@IncomeDate", incomedate.Value.Date);
-                    command.Parameters.AddWithValue("@IncomeDescription", incomedescription.Text);
-                    command.Parameters.AddWithValue("@IncomeUser", Form6.User);
-                    command.ExecuteNonQuery();
-                    MessageBox.Show("Income added correctly!");
-                    connection.Close();
-                    Clear();
+            //if (expensename.Text == " " || incomeamount.Text == " " || incomecategories.SelectedIndex == -1 || incomedescription.Text == "")
+            //{
+            //    MessageBox.Show("Complete the data!");
+            //}
+            //else
+            //{
+            //    try
+            //    {
+            //        connection.Open();
+            //        SqlCommand command = new SqlCommand("insert into tbl_Income(Income_name, Income_Amount, Income_Category, Income_Date, Income_Description, Income_User)values (@IncomeName, @IncomeAmount, @IncomeCategory, @IncomeDate, @IncomeDescription, @IncomeUser)", connection);
+            //        command.Parameters.AddWithValue("@IncomeName", incomename.Text);
+            //        command.Parameters.AddWithValue("@IncomeAmount", incomeamount.Text);
+            //        command.Parameters.AddWithValue("@IncomeCategory", incomecategories.SelectedItem.ToString());
+            //        command.Parameters.AddWithValue("@IncomeDate", incomedate.Value.Date);
+            //        command.Parameters.AddWithValue("@IncomeDescription", incomedescription.Text);
+            //        command.Parameters.AddWithValue("@IncomeUser", Form6.User);
+            //        command.ExecuteNonQuery();
+            //        MessageBox.Show("Income added correctly!");
+            //        connection.Close();
+            //        Clear();
 
                 }
-                catch (Exception Ex)
-                {
-                    MessageBox.Show(Ex.Message);
+                //catch (Exception Ex)
+                //{
+                //    MessageBox.Show(Ex.Message);
 
-                }
-            }
-        }
+                //}
+        //    }
+        //}
     }
 }
